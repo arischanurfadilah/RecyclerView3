3175410ef864ded35663570235a1acb95a3d09c9
Arischa Nur Fadilah <ariskanf12@gmail.com>
2016-11-04 13:10:34 +0700

    RecyclerView View Add

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -26,6 +26,7 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter{
 
+    private static final int REQUEST_CODE_ADD = 88;
     public static String HOTEL;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
@@ -41,10 +42,10 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goAdd();
             }
         });
+
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -54,6 +55,11 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
         fillData();
     }
+
+    private void goAdd() {
+        startActivityForResult(new Intent(this,InputActivity.class), REQUEST_CODE_ADD);
+    }
+
     private void fillData() {
         Resources resources = getResources();
         String[] arJudul = resources.getStringArray(R.array.places);
@@ -105,5 +111,15 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         intent.putExtra(HOTEL,mList.get(pos));
         startActivity(intent)
         ;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK){
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
